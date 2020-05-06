98f4cd08aa5852f3d45bbea4816841cb6349e714
Esmeralda Hdez <esmeyhg@gmail.com>
2020-05-05 21:26:45 -0500

    Base final

--- Asistencias/Asistencias/settings.py
+++ Asistencias/Asistencias/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'Usuarios',
     'crispy_forms',
+    'bootstrap4',
     'bootstrap_datepicker_plus'
 ]
 
--- Asistencias/Usuarios/urls.py
+++ Asistencias/Usuarios/urls.py
@@ -7,15 +7,6 @@ from django.conf import settings
 from django.conf.urls import url,include    
 app_name="Usuarios"
 
-from django.contrib import admin
-from django.urls import path
-from .views import *
-from django.contrib.auth.decorators import login_required
-from django.views.static import serve
-from django.conf import settings
-from django.conf.urls import url,include    
-app_name="Usuarios"
-
 urlpatterns = [
     path('login/', ingresar),
     path('registro/', registrar),
